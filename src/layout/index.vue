f65c467de224112d48395be96096cb27c7116b95
dbz0423 <2569876934@qq.com>
2024-11-08 17:56:49 +0800

    feat: 选项卡关闭标签制作

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -8,7 +8,10 @@
         <Header></Header>
       </el-header>
       <el-main class="main">
-        <Tabs></Tabs>
+        <div class="tabs">
+          <Tabs class="tabs-content"></Tabs>
+          <CloseTabs></CloseTabs>
+        </div>
         <router-view></router-view>
       </el-main>
     </el-container>
@@ -19,6 +22,7 @@
 import Header from "@/layout/header/Header.vue";
 import MenuBar from "@/layout/menu/MenuBar.vue";
 import Tabs from "@/layout/tabs/Tabs.vue";
+import CloseTabs from "@/layout/tabs/CloseTabs.vue";
 </script>
 <style lang="scss" scoped>
 .mycontainer {
@@ -32,6 +36,23 @@ import Tabs from "@/layout/tabs/Tabs.vue";
     background-color: #009688;
   }
   .main {
+    padding: 0px;
+    .tabs {
+      display: flex;
+      justify-content: space-between;
+      flex-grow: 1;
+      border-bottom: 1px solid #e4e7ed;
+      padding-right: 75px;
+      .el-tabs {
+        width: 100%;
+        height: 33px;
+      }
+      .tabs-content {
+        padding-top: 8px;
+        padding-left: 10px;
+        padding-right: 10px;
+      }
+    }
     background-color: #ffffff;
   }
 }
